036e9c39636de0d3b92560848e28e509a1bb0944
Mainur Rahman <enan.mainur@gmail.com>
2021-12-22 17:55:39 +0600

    updated

--- timeProblem.js
+++ timeProblem.js
@@ -11,26 +11,17 @@ const inputTime='12:01:15AM';
 // const inputTime='11:16:00AM';
 // const inputTime='12:01:00AM';
 
-console.log('-----------------------------------')
-console.log(`--------initial ${inputTime}----------`)
-console.log('-----------------------------------')
-
 let [hour,minute,second] = inputTime.slice(0,8).split(':')
 let day= inputTime.slice(8,inputTime.length)
-console.log(day);
 let newHour;
 let newMinute;
 let newSecond;
 
-console.log(typeof(parseInt(hour)),minute,second)
-
 // adding 45 minute and 45 second with the time
 if(parseInt(second)>=15){
-    console.log('inside second')
     second=(parseInt(second)+45)-60;
     minute=parseInt(minute)+1;
     if(minute>=15){
-        console.log('inside minute')
         minute=(parseInt(minute)+45)-60;
         hour=parseInt(hour)+1
         if(hour>12){
@@ -54,7 +45,6 @@ if(parseInt(second)>=15){
     }
 }
 else if(parseInt(minute)>=15){
-    console.log('inside outer minute')
     minute=(parseInt(minute)+45)-60;
     hour=parseInt(hour)+1
     if(hour>12){
@@ -81,54 +71,41 @@ else if(minute<15 && second<15){
     second=parseInt(second)+45;
 }
 
-console.log('-----------------------------------')
-console.log(`--------changed ${hour}:${minute}:${second}${day}----------`)
-console.log('-----------------------------------')
-
 // converting the time to military time
 if(day=='PM' && hour<=12){
-    console.log('ok it is PM')
     if(hour == 12){
         newHour=parseInt(hour)
         newMinute=parseInt(minute)
         newSecond=parseInt(second)
-        console.log(`1 PM newhour ${newHour} ${newMinute} ${newSecond}`)
     }
     else if(hour!=12){
         newHour=12+parseInt(hour)
         newMinute=minute
         newSecond=second
-        console.log(`2 PM newHour ${newHour} ${newMinute} ${newSecond}`)
     }
 }
-if(day=='AM' && hour<=12){
-    console.log('ok it is AM')
+else if(day=='AM' && hour<=12){
     if(hour == 12){
         newHour='00'
         newMinute=minute
         newSecond=second
-        console.log(`3 AM newhour ${newHour} ${newMinute} ${newSecond}`)
     }
     else if(hour!=12 && hour<10){
         newHour='0'+`${00+parseInt(hour)}`
         newMinute=minute
         newSecond=second
-        console.log(`4 AM newHour ${newHour} ${newMinute} ${newSecond}`)
     }
     else{
         newHour=00+parseInt(hour)
         newMinute=minute
         newSecond=second
-        console.log(`5 AM newHour ${newHour} ${newMinute} ${newSecond}`)
     }
 }
 
-console.log(newHour,newMinute,newSecond)
 
 // formatting the time 
 if(`${newHour}`.length<2){
     newHour='0'+newHour
-    console.log('updating, newHour')
 }
 if(`${newMinute}`.length<2){
     newMinute='0'+newMinute
